3a40616ae47fcba4e1f56960f68a77f90b21f721
FallingSakura <dbw1732351264@163.com>
2024-09-17 00:21:22 +0800

    divided

--- src/router/index.js
+++ src/router/index.js
@@ -8,21 +8,6 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/calendar',
-    name: 'calendar',
-    component: () => import('../views/Calendar/CalendarView.vue')
-  },
-  {
-    path: '/calendar/login',
-    name: 'calendar-login',
-    component: () => import('../views/Calendar/CalendarLoginView.vue')
-  },
-  {
-    path: '/calendar/rigister',
-    name: 'calendar-rigister',
-    component: () => import('../views/Calendar/CalendarRigisterView.vue')
-  }
 ]
 Routes.forEach((route) => {
   routes.push(route)
